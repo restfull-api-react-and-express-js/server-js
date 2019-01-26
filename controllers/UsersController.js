93f8155b8c86c390e8d1596c60dd227db348ad73
elfinsanjaya12 <elfinsanjaya1207@gmail.com>
2019-01-26 16:43:27 +0700

    Proses Uodate Data Users

--- controllers/UsersController.js
+++ controllers/UsersController.js
@@ -64,5 +64,58 @@ module.exports = {
           });
         }
       });
+  },
+  update: (req, res) => {
+    const { id } = req.params;
+    const { email, password, name, role } = req.body;
+    const hashPassword = bcrypt.hashSync(password, 10);
+    models.User.findOne({
+      where: { id: id }
+    })
+      .then(user => {
+        if (user) {
+          user
+            .update({
+              email,
+              password: hashPassword,
+              name,
+              role
+            })
+            .then(updatedUser => {
+              res.status(200).json({
+                message: "Success Update User",
+                data: user
+              });
+            })
+            .catch(err => {
+              if (err.errors[0].message) {
+                const message = err.errors[0].message;
+                res.status(403).json({
+                  message: message
+                });
+              } else {
+                res.status(500).json({
+                  message: "Something Went Wrong"
+                });
+              }
+            });
+        } else {
+          res.status(404).json({
+            message: "User Not Found"
+          });
+        }
+      })
+      .catch(err => {
+        if (err.errors[0].message) {
+          const message = err.errors[0].message;
+          res.status(403).json({
+            message: message
+          });
+        } else {
+          res.status(500).json({
+            message: "Something Went Wrong"
+          });
+        }
+      });
   }
 };
--- routes/users.js
+++ routes/users.js
@@ -1,9 +1,10 @@
 const express = require("express");
 const router = express.Router();
-const { all, find, create } = require("../controllers/UsersController");
+const { all, find, create, update } = require("../controllers/UsersController");
 
 router.get("/", all);
 router.get("/:id", find);
 router.post("/create", create);
+router.put("/:id", update);
 
 module.exports = router;
