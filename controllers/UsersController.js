e8e05ca4a0b631cfd90df296eafa4e4daf85bcb5
elfinsanjaya12 <elfinsanjaya1207@gmail.com>
2019-01-26 18:30:58 +0700

    Signin User

--- controllers/UsersController.js
+++ controllers/UsersController.js
@@ -117,5 +117,68 @@ module.exports = {
           });
         }
       });
+  },
+  destroy: (req, res) => {
+    const { id } = req.params;
+    models.User.findOne({
+      where: {
+        id: id
+      }
+    })
+      .then(user => {
+        user
+          .destroy()
+          .then(() => {
+            res.status(200).json({
+              message: "Success Delete User",
+              data: user
+            });
+          })
+          .catch(err => {
+            res.status(500).json({
+              message: "Something Went Wrong"
+            });
+          });
+      })
+      .catch(err => {
+        res.status(500).json({
+          message: "Something Went Wrong"
+        });
+      });
+  },
+  signin: (req, res) => {
+    const { email, password } = req.body;
+    models.User.findOne({
+      where: {
+        email: email
+      }
+    }).then(user => {
+      if (user) {
+        const checkPassword = bcrypt.compareSync(password, user.password); // true
+        if (checkPassword) {
+          const token = jwt.sign(
+            {
+              user: {
+                id: user.id,
+                email: user.email
+              }
+            },
+            "secret"
+          );
+          res.status(200).json({
+            message: "Success Signin",
+            data: { token, role: user.role }
+          });
+        } else {
+          res.status(403).json({
+            message: "Invalid Signin"
+          });
+        }
+      } else {
+        res.status(403).json({
+          message: "Invalid Signin"
+        });
+      }
+    });
   }
 };
--- routes/users.js
+++ routes/users.js
@@ -1,10 +1,19 @@
 const express = require("express");
 const router = express.Router();
-const { all, find, create, update } = require("../controllers/UsersController");
+const {
+  all,
+  find,
+  create,
+  update,
+  destroy,
+  signin
+} = require("../controllers/UsersController");
 
 router.get("/", all);
 router.get("/:id", find);
 router.post("/create", create);
 router.put("/:id", update);
+router.delete("/:id", destroy);
+router.post("/signin", signin);
 
 module.exports = router;
