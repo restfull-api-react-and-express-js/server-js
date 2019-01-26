8569b6c1d0f91172d580057ff10aca8d87e243cb
elfinsanjaya12 <elfinsanjaya1207@gmail.com>
2019-01-26 16:30:07 +0700

    Proses Create Users

--- controllers/UsersController.js
+++ controllers/UsersController.js
@@ -36,5 +36,33 @@ module.exports = {
           message: "Something Went Wrong"
         });
       });
+  },
+  create: (req, res) => {
+    const { email, name, password, role } = req.body;
+    const hashPassword = bcrypt.hashSync(password, 10);
+    models.User.create({
+      email,
+      password: hashPassword,
+      name,
+      role
+    })
+      .then(user => {
+        res.status(201).json({
+          message: "Success Create User",
+          data: user
+        });
+      })
+      .catch(err => {
+        if (err.errors[0].message) {
+          const message = err.errors[0].message;
+          res.status(403).json({
+            message: message
+          });
+        } else {
+          res.status(500).json({
+            message: "Something Went Wrong"
+          });
+        }
+      });
   }
 };
--- routes/users.js
+++ routes/users.js
@@ -1,8 +1,9 @@
 const express = require("express");
 const router = express.Router();
-const { all, find } = require("../controllers/UsersController");
+const { all, find, create } = require("../controllers/UsersController");
 
 router.get("/", all);
 router.get("/:id", find);
+router.post("/create", create);
 
 module.exports = router;
